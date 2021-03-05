ae7e7666842b70ee537afd73d50b0a2cb56edddc
Michael Thoreau <michaelthoreau@Michaels-MacBook-Pro.local>
2021-03-05 18:22:07 -0500

    first commit:

---
+++ src/main.cpp
@@ -0,0 +1,40 @@
+#include <mbed.h>
+#include <USBSerial.h>
+
+USBSerial serial;
+
+// reference: http://www.ethernut.de/en/documents/arm-inline-asm.html
+
+//asm(code : output operand list : input operand list : clobber list);
+
+
+int main(void)
+{
+  while (1) {
+    uint32_t x = 1;
+    uint32_t y = 5;
+    
+    // swap x and y
+    // asm(  "mov r0, %[value]\n\t"
+    //       "mov %[result], r0"
+    // : [result] "=r" (y) 
+    // : [value] "r" (x): "r0");
+
+    // add 1 to x until it is equal to y
+    asm(  "mov r0, %[value]\n\t"
+          "mov r1, %[threshold]\n\t"
+          "loop:\n\t"
+          "cmp r0, r1\n\t"
+          "beq end\n\t"
+          "add r0, #1\n\t"
+          "b loop\n\t"
+          "end:\n\t"
+    :
+    : [threshold] "r" (y), [value] "r" (x)
+    : "r0", "r1", "cc");
+
+
+    serial.printf("x: %d  y: %d\n", x, y);
+  }
+  
+}
