3d1f87f4797640dc8233ab5cf6ea7ca91f0b520a
Michael Thoreau <michaelthoreau@10-18-180-220.dynapool.wireless.nyu.edu>
2021-03-09 15:19:50 -0500

    finished demo

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,14 @@
 
 USBSerial serial;
 
+
 // reference: http://www.ethernut.de/en/documents/arm-inline-asm.html
 // also:      https://azeria-labs.com/downloads/cheatsheetv1.3-1920x1080.png
-
-//asm(code : output operand list : input operand list : clobber list);
 
 
 extern "C" uint32_t add2(uint32_t a, uint32_t b);
 extern "C" uint32_t bad_add(uint32_t a, uint32_t b);
 
-
-
-uint32_t add2_func(uint32_t a, uint32_t b) {
-	return a+b;
-}
 
 int main(void)
 {
@@ -25,27 +19,30 @@ int main(void)
 	uint32_t y = 5;
 	uint32_t result;
 	while (1) {
-		
-		/* add 2 numbers in c */
-		result = add2_func(x,y);
+		result = x+y;
 		serial.printf("c prog - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
 
-		/* add 2 numbers in assembly - inline */
-		asm(
-		"add %[out], %[val1], %[val2]\n\t"
-		: [out] "=r" (result)
-		: [val1] "r" (x), [val2] "r" (y)
-		:
-		);
-		serial.printf("inline - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
-
-		/* add 2 numbers in assembly - external*/
 		result = add2(x,y);
-		serial.printf("extern - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
+		serial.printf("assembly - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
 
-		/* add 2 numbers in assembly - external*/
+		//asm(code : output operand list : input operand list : clobber list);
+
+		asm("mov r0, %[val1]\n\t"
+			"add %[out], r0, %[val2]\n\t"
+		:[out] "=r" (result)
+		:[val1] "r" (x), [val2] "r" (y)
+		:"r0"
+		);
+
+		asm("mov r0, r0");
+		asm("nop");
+		serial.printf("assembly2 - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
+
+
 		result = bad_add(x,y);
-		serial.printf("bad_add - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
+		serial.printf("bad asm - x: %lu   y: %lu   result: %lu\n\r", x, y, result);
+
+
 	}
 
 }
